7d1d62c9da65c4c2b5837d521a149cc23d6700db
electric coffee <slench102@gmail.com>
2017-01-08 14:51:48 +0100

    add convert_vec

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,34 @@ fn negation_partner(val: u32) -> u32 {
     }
 }
 
+fn convert_vec(vec: Vec<u32>) -> Option<u32> {
+    let mut last = 0;
+    let mut acc = 0;
+    for current in vec  {
+        // if the last value is less than the current value, attempt subtraction
+        if last < current && last != 0 {
+            let partner = negation_partner(current);
+            // if the last value is valid, subtract then increment
+            // making sure to reset the last value to 0
+            if partner == last {
+                // remove the last added value
+                acc -= last;
+                // then add the last value subtracted from the current
+                acc += current - last;
+                last = 0;
+            } else {
+                return None;
+            }
+        // otherwise, if the last value is greater, or 0, increment
+        // making sure to bump the last value to the current one
+        } else {
+            acc += current;
+            last = current;
+        }
+    }
+    Some(acc)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
